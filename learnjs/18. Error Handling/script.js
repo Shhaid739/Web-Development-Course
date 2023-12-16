28a8ddeb849df334f34bcda01e7cec673d5054f1
Shhaid739 <153923117+Shhaid739@users.noreply.github.com>
2023-12-16 19:14:16 +0530

    Update script.js

--- learnjs/18. Error Handling/script.js
+++ learnjs/18. Error Handling/script.js
@@ -13,7 +13,8 @@
 
 try {
     console.log('before the error')
-    const age = 12;
+    //const age = 12;
+    /age commented,error occured then directly run catch block
     console.log(age);
     console.log('after the error')
 } catch (err) {
